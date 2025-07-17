2ea9ef52718173293e46ac9851d6e7e88093563f
José Quintinno <josequintino@hotmail.com.br>
2025-07-16 22:31:59 -0300

    feature: Configurar Gateway Server

---
+++ src/main/java/br/com/quintinno/portariumapi/configuration/GatewayConfiguration.java
@@ -0,0 +1,17 @@
+package br.com.quintinno.portariumapi.configuration;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfiguration {
+
+	@Bean
+	RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route("defensium-service", route -> route.path("/defensium/**").uri("http://localhost:8080")).build();
+	}
+
+}
